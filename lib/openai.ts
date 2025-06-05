40ea4b8fc40f9570819dff44585ae867050271b3
hayshin <hayshinbj@gmail.com>
2025-06-05 22:11:21 +0500

    try to fix openai

--- lib/openai.ts
+++ lib/openai.ts
@@ -4,7 +4,7 @@ import { useAIChatStore } from "@/store/useAIChatStore";
 function getApiKey() {
   return process.env.OPENAI_API_KEY || process.env.NEXT_PUBLIC_OPENAI_API_KEY || "your api key here";
 }
-const openai = new OpenAI({apiKey: getApiKey(), dangerouslyAllowBrowser: true});
+const openai = new OpenAI({apiKey: getApiKey()});
 
 export async function createResponse(input: string, chatname: string, model = "gpt-4o-mini", ) {
     const lastId = useAIChatStore.getState().getLastResponseId("alice");
